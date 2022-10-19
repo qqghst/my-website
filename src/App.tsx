c6ba12915f293ff2bc6aee7a62b84d14f48fdca0
Dmitry <theicesilver121@mail.ru>
2022-10-19 21:50:06 +0300

    add loader (not final version)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, CSSProperties } from 'react';
+// import ClipLoader from "react-spinners/ClipLoader";
 import NavBar from './Pages/NavBar';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import RouteAbout from './Pages/RouteAbout';
 import RouteProjects from './Pages/RouteProjects';
 import Main from './Pages/Main';
+import Loader from './Components/Loader/loader';
 
+// const override: CSSProperties = {
+//     display: "block",
+//     margin: "0 auto",
+//     borderColor: "blue",
+// };
 
 function App() {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false)
+        }, 4000)
+    }, [])
     return (
         <div className=''>
-            <BrowserRouter>
-                <NavBar />
-                <Routes>
-                    <Route path='/' element={< Main />} />
-                    <Route path='/hero' element={< Main />} />
-                    <Route path='/about' element={< RouteAbout />} />
-                    <Route path='/projects' element={< RouteProjects />} />
-                </Routes>
-            </BrowserRouter>
+            {
+                loading ?
+
+                    <Loader/>
+
+                    :
+
+                    <BrowserRouter>
+                        <NavBar />
+                        <Routes>
+                            <Route path='/' element={< Main />} />
+                            <Route path='/hero' element={< Main />} />
+                            <Route path='/about' element={< RouteAbout />} />
+                            <Route path='/projects' element={< RouteProjects />} />
+                        </Routes>
+                    </BrowserRouter>
+            }
         </div>
     );
 }
