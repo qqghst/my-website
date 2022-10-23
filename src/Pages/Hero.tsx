0b623b6a744067621028822da812de097882c95f
Dmitry <theicesilver121@mail.ru>
2022-10-23 14:46:06 +0300

    fixing code

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -27,9 +27,10 @@ const Hero: React.FC = () => {
                     </div> */}
                 </div>
                 <div className='container mx-auto'>
-                    <div className='absolute bottom-[70px] flex'>
+                    <div className='absolute bottom-[70px] '>
                         <Button onClick={handleClick} type='button'>
-                            <span className='font-jetBrains bg-gradient-to-r bg-clip-text text-transparent from-whiteOppacity to-whiteOppacity hover:from-[#224078] hover:to-[#A42C7B]'>keep going?</span>
+                            {/* <span className='font-jetBrains bg-gradient-to-r bg-clip-text text-transparent from-whiteOppacity to-whiteOppacity hover:from-[#672BE5] hover:to-[#672BE5]'>keep going?</span> */}
+                            <span className='font-jetBrains hover:text-[#672BE5]'>keep going?</span>
                             <img
                                 className='px-[16px] animate-bounce'
                                 src={arrow}
