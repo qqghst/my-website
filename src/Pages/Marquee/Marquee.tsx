85a5dacb7d3c8d3152c50c67382cfb0c4bd4766c
Dmitry <theicesilver121@mail.ru>
2022-11-05 13:59:50 +0300

    deleted dependencies fast marquee and spinners

--- src/Pages/Marquee/Marquee.tsx
+++ src/Pages/Marquee/Marquee.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Marquee from 'react-fast-marquee';
-import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import style from './Marquee.module.scss';
 
@@ -20,24 +18,6 @@ const marqueeVariants = {
 
 const ScrollHorizontal: React.FC = () => {
     return (
-        <>
-            {/* <section className='flex flex-col justify-center'>
-                <div className='flex flex-col justify-center items-center my-[40px]'>
-                    <Marquee speed={90} gradient={false}>
-                        <ul className={`flex  text-whiteOppacity32 font-bold tracking-bigText text-[40px] sm:text-[57.33px] transition-opacity`}>
-                            <li className=''>
-                                <Link className='hover:text-whiteOppacity' to='#'>keep in touch&nbsp;/</Link>
-                            </li>
-                            <li className=''>
-                                <Link className='hover:text-whiteOppacity' to='#'>&nbsp;theicesilver121@mail&nbsp;/</Link>
-                            </li>
-                            <li className=''>
-                                <Link className='hover:text-whiteOppacity' to='#'>&nbsp;tg @qqghst</Link>
-                            </li>
-                        </ul>
-                    </Marquee>
-                </div>
-            </section> */}
             <div>
                 <div className={`${style.marquee}`}>
                     <motion.div
@@ -51,7 +31,6 @@ const ScrollHorizontal: React.FC = () => {
                     </motion.div>
                 </div>
             </div>
-        </>
     )
 }
 
